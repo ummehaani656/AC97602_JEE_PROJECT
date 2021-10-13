0dccb1543a2a34d8c2b4a5aaa178e95155e3785f
ummehaani656 <ummehaani656@gmail.com>
2021-10-13 20:14:58 +0530

    lab1

---
+++ lab1/src/com/training/Application.java
@@ -0,0 +1,14 @@
+package com.training;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      CurrencyConverter obj =new CurrencyConverter();
+       double Dollar=obj.inrTousd(5000);
+      System.out.println(Dollar);
+	}
+
+	
+
+}
---
+++ lab1/src/com/training/CurrencyConverter.java
@@ -0,0 +1,9 @@
+package com.training;
+
+public class CurrencyConverter {
+	public double  inrTousd(double inr)
+	{
+		return inr/(73.69);
+	}
+
+}
